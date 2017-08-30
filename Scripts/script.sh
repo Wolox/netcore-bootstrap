2317413aabe28e91797c61eb889e28c40159f666
mtrucco <truccomarcos@gmail.com>
2017-08-30 18:18:39 -0300

    Small fix in script.sh

--- Scripts/script.sh
+++ Scripts/script.sh
@@ -1,4 +1,4 @@
-dotnet bootstrap-script.dll $1
+dotnet ./netcore-bootstrap/Scripts/bootstrap-script.dll $1
 if test "$?" = '1' ; then
 	chmod +x ./netcore-bootstrap/Scripts/delete_script.sh
 	./netcore-bootstrap/Scripts/delete_script.sh
