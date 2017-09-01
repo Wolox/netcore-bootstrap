04922edba43d02cb4f51e784ba59402e6a1ea22f
Gustavo Siciliano <gussiciliano@gmail.com>
2017-09-01 16:29:01 -0300

    fixes for review

--- Scripts/Src/Program.cs
+++ Scripts/Src/Program.cs
@@ -1,46 +1,45 @@
 ﻿using System;
 using System.IO;
 
-namespace bootstrap_script
+namespace BootstrapScript
 {
     class Program
     {
         static int Main(string[] args)
         {
-            string AppName = args[0];
-            Console.WriteLine("Setting up bootstrap for " + AppName);
-            string BootstrapRootDir = "";
-            string BootstrapName = "NetCoreBootstrap";
-            foreach (var Dir in Directory.GetCurrentDirectory().Split('/'))
+            string appName = args[0];
+            Console.WriteLine("Setting up bootstrap for " + appName);
+            string bootstrapRootDir = "";
+            string bootstrapName = "NetCoreBootstrap";
+            foreach (var dir in Directory.GetCurrentDirectory().Split('/'))
             {
-                BootstrapRootDir += Dir + "/";
-                if (Dir == AppName) break;
+                bootstrapRootDir += dir + "/";
+                if (dir == appName) break;
             }
-            Console.WriteLine("Replacing " + BootstrapName + " to " + AppName + " in " + BootstrapRootDir);
-            string Contents = "";
-            foreach (string file in Directory.EnumerateFiles(BootstrapRootDir, "*", SearchOption.AllDirectories))
+            Console.WriteLine("Replacing " + bootstrapName + " to " + appName + " in " + bootstrapRootDir);
+            string contents = "";
+            foreach (string file in Directory.EnumerateFiles(bootstrapRootDir, "*", SearchOption.AllDirectories))
             {
-                if (file.Contains("bootstrap-script"))
-                    continue;
-                Contents = File.ReadAllText(file);
-                if (Contents.Contains(BootstrapName))
+                if (file.Contains("bootstrap-script")) continue;
+                contents = File.ReadAllText(file);
+                if (contents.Contains(bootstrapName))
                 {
-                    Contents = Contents.Replace(BootstrapName, AppName);
+                    contents = contents.Replace(bootstrapName, appName);
                     if (file.Contains("/README.md"))
                     {
-                        Contents = Contents.Replace("### [Kickoff] Application Setup\n","")
+                        contents = contents.Replace("### [Kickoff] Application Setup\n","")
                                             .Replace("After cloning the bootstrap, run `chmod +x ./Scripts/script.sh`\n","")
                                             .Replace("Then `./Scripts/script.sh AppName` where `AppName` is your application name.\n","")
                                             .Replace("Your app is ready. Happy coding!\n","");
                     }
-                    File.WriteAllText(file, Contents);
+                    File.WriteAllText(file, contents);
                 }
             }   
             Console.WriteLine("Renaming .csproj ...");
-            File.Move(BootstrapRootDir + BootstrapName + ".csproj", BootstrapRootDir + AppName + ".csproj");
+            File.Move(bootstrapRootDir + bootstrapName + ".csproj", bootstrapRootDir + appName + ".csproj");
             Console.WriteLine("Moving appsettings.Development.json ...");
-            File.Move(BootstrapRootDir + "Scripts/appsettings.Development.json", BootstrapRootDir + "appsettings.Development.json");
-            Console.WriteLine(AppName + " is ready! Happy coding!");
+            File.Move(bootstrapRootDir + "Scripts/appsettings.Development.json", bootstrapRootDir + "appsettings.Development.json");
+            Console.WriteLine(appName + " is ready! Happy coding!");
             return 1;
         }
     }    
