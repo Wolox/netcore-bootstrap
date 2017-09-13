41d3fc3024eeb9d614ebcfae0898e44974dc06d9
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-09-13 18:03:26 -0300

    Little fix

--- Models/Views/RoleManagerViewModel.cs
+++ Models/Views/RoleManagerViewModel.cs
@@ -7,7 +7,6 @@ namespace NetCoreBootstrap.Models.Views
     public class RoleManagerViewModel
     {
         public List<SelectListItem> UsersListItem { get; set; }
-        public List<string> CurrentRoles { get; set; }
         public Dictionary<string, bool> Roles { get; set; }
         public string SelectedUserId { get; set; }
     }
