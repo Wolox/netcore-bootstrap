2c3c5b9f25f42d9ac84c6b260349e69c1d863fb6
Gustavo Siciliano <gussiciliano@gmail.com>
2017-12-07 13:22:59 -0300

    more little fixes

---
+++ NetcoreBootstrap.Tests/Controllers/api/v1/HomeApiTestController.cs
@@ -0,0 +1,48 @@
+using System;
+using Xunit;
+using NetcoreBootstrap;
+using System.Net.Http;
+using NetCoreBootstrap;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace NetcoreBootstrap.Tests.Controllers.api.v1
+{
+    public class HomeApiTestController
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public HomeApiTestController()
+        {
+            var configuration = new ConfigurationBuilder()
+                                .SetBasePath(Path.GetFullPath(@"../../../../NetcoreBootstrap/"))
+                                .AddJsonFile("appsettings.Development.json", optional: false)
+                                .Build();
+            this._server = new TestServer(new WebHostBuilder()
+                            .UseStartup<Startup>()
+                            .UseConfiguration(configuration));
+            this._client = this._server.CreateClient();
+        }        
+
+        public HttpClient Client
+        {
+            get { return this._client; }
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            // Act
+            var response = await Client.GetAsync("/api/v1/homeapi/1");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal("value", responseString);
+        }
+    }
+}
