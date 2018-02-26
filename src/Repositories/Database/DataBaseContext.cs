7807c03cfa5f0783c1f932a05f14fa8afd576d0b
Marcos Trucco <truccomarcos@gmail.com>
2018-02-26 13:13:00 -0300

    One more small change

--- src/Repositories/Database/DataBaseContext.cs
+++ src/Repositories/Database/DataBaseContext.cs
@@ -6,12 +6,11 @@ using NetCoreBootstrap.Models.Database;
 
 #endregion
 
-namespace NetCoreBootstrap.Persistance
+namespace NetCoreBootstrap.Persistance.Database
 {
     public class DataBaseContext : IdentityDbContext<User>
     {
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
-        public virtual DbSet<ExampleModel> ExampleModels { get; set; } // TODO DELETE THIS LINE
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
