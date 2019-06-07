97adbe2594ec2abc1c641603923c6d418aa560cc
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-06-07 18:36:38 -0300

    Added users VO

---
+++ NetCoreBootstrap.Core/Models/VOs/UserSignInVO.cs
@@ -0,0 +1,9 @@
+namespace NetCoreBootstrap.Core.Models.VOs
+{
+    public class UserSignInVO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
