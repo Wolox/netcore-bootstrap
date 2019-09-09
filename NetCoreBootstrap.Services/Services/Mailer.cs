e43be41fd860008d0d3776f329194fd40c4d7a4b
mikolaslorant <lmikolas@itba.edu.ar>
2019-09-09 18:05:12 -0300

    added mailkit

--- NetCoreBootstrap.Services/Services/Mailer.cs
+++ NetCoreBootstrap.Services/Services/Mailer.cs
@@ -1,12 +1,8 @@
 using System;
-using System.IO;
-using System.Net;
-using System.Net.Mail;
-using System.Text;
+using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
+using MimeKit;
 using NetCoreBootstrap.Services.Intefaces;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace NetCoreBootstrap.Services
 {
@@ -14,6 +10,8 @@ namespace NetCoreBootstrap.Services
     {
         private string _host, _username, _password, _name, _email;
         private int _hostPort;
+        private readonly SmtpClient _client;
+        private readonly MailboxAddress _fromAddress;
 
         public Mailer(IConfiguration configuration)
         {
@@ -23,6 +21,10 @@ namespace NetCoreBootstrap.Services
             _password = configuration["Mailer:Password"];
             _name = configuration["Mailer:Name"];
             _email = configuration["Mailer:Email"];
+            _fromAddress = new MailboxAddress(_name, _email);
+            _client = new SmtpClient();
+            _client.Connect(_host, _hostPort, false);
+            _client.Authenticate(_username, _password);
         }
 
         public string Host => _host;
@@ -32,23 +34,25 @@ namespace NetCoreBootstrap.Services
         public string Name => _name;
         public string Email => _email;
 
+        public MailboxAddress FromMailbox => _fromAddress;
+
+
         public void SendMail(string toAddress, string subject, string body, bool isHtml = true)
         {
-            SmtpClient client = new SmtpClient(Host, HostPort)
+            var message = new MimeMessage();
+            message.From.Add(FromMailbox);
+            message.To.Add(new MailboxAddress(toAddress));
+            message.Subject = subject;
+            var builder = new BodyBuilder();
+            builder.TextBody = body;
+            message.Body = builder.ToMessageBody();
+            using (var client = new SmtpClient())
             {
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(Username, Password),
-                EnableSsl = true,
-            };
-            MailMessage mailMessage = new MailMessage()
-            {
-                From = new MailAddress(Email, Name),
-                Body = body,
-                Subject = subject,
-                IsBodyHtml = isHtml,
-            };
-            mailMessage.To.Add(toAddress);
-            client.Send(mailMessage);
+                client.Connect(Host, HostPort, false);
+                client.Authenticate(Username, Password);
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
     }
 }
