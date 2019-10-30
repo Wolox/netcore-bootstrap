e5e5ccf451740c812d915586ac21f98108baac20
Federico Fasitella <federico.fasitella@wolox.com.ar>
2019-10-30 12:15:37 -0300

    Metrics script

---
+++ script.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import argparse
+import shutil
+import contextlib
+import io
+import sys
+
+bootstrap_name = "NetCoreBootstrap"
+folder_list = ["Api", "Core", "Data", "Services", "Tests"]
+parent_folder = ".."
+ignored_folders = [".git", "bin", "obj"]
+empty_string = ""
+end_of_line = "\r\n"
+if_word = "if ("
+and_word = "&&"
+or_word = "||"
+else_word = "else"
+else_if_word = "else if ("
+foreach_word = "foreach ("
+case_word = "case"
+
+################### Helpers ###################
+class Fonts:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    OKGREEN = '\033[1m\033[92m'
+    WARNING = '\033[1m\033[93m'
+    FAIL = '\033[1m\033[91m'
+
+class Module:
+    def __init__(self, project_name, direct_dependencies):
+        self.project_name = project_name
+        self.direct_dependencies = direct_dependencies
+
+class Build:
+    def __init__(self, time_elapsed, build_errors, warnings):
+        self.warnings = warnings
+        self.build_errors = build_errors
+        self.time_elapsed = time_elapsed
+
+def __get_files():
+    files = []
+    for root, directories, filenames in os.walk(os.curdir):
+        for filename in filenames:
+            if not (__folder_must_be_ignored(root) or filename == 'kickoff.py' or filename == 'script.py'):
+                files.append(os.path.join(root, filename))
+    return files
+
+def __get_csproj_files():
+    files = []
+    for root, directories, filenames in os.walk(os.curdir):
+        for filename in filenames:
+            if filename.endswith('.csproj'):
+                files.append(os.path.join(root, filename))
+    return files
+
+def __folder_must_be_ignored(folder_name):
+    for folder in ignored_folders:
+        if folder in folder_name:
+            return True
+    return False
+
+def __trim_project_name(path):
+    name = str(path).replace('<ProjectReference Include=".', '')
+    name = name.replace(' />', '')
+    return name
+    
+################### Metrics ###################
+
+# Cyclomatic complexity
+def calculate_complexity():
+    files = __get_files()
+    complexity = 0
+    for file_name in files:
+        file = open(file_name, 'r')
+        content = file.read()
+        if bootstrap_name in content:
+            complexity += content.count(if_word)
+            complexity += content.count(and_word)
+            complexity += content.count(or_word)
+            complexity += content.count(else_if_word)
+            complexity += content.count(else_word)
+            complexity += content.count(foreach_word)
+            complexity += content.count(case_word)
+        file.close()
+    return complexity
+
+# Direct dependencies
+def get_direct_dependencies():
+    csproj_files = __get_csproj_files()
+    modules = []
+    for file_name in csproj_files:
+        direct_dependencies = []
+        file = open(file_name, 'r')
+        content = file.readlines()
+        for line in content:
+            if (bootstrap_name in line):
+                direct_dependencies.append(__trim_project_name(line))
+        modules.append(Module(__trim_project_name(file_name), direct_dependencies))
+        file.close()
+    return modules
+
+
+# Code coverage
+def code_coverage():
+    print('Running code coverage analysis...')
+    os.chdir(bootstrap_name + ".Tests")
+    test_command = 'dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./TestResults/ ./NetCoreBootstrap.Tests.csproj >/dev/null 2>&1'
+    coverage_report = 'reportgenerator -reports:"./TestResults/coverage.opencover.xml"  -targetdir:"./TestResults/Coverage/Reports" -reportTypes:TextSummary >/dev/null 2>&1'
+    grep = 'grep "Line coverage" ./TestResults/Coverage/Reports/Summary.txt'
+    os.system(test_command)
+    os.system(coverage_report)
+    coverage = os.popen(grep).read()
+    os.chdir(parent_folder)
+    print('Removing files')
+    os.remove(os.curdir + "/" + bootstrap_name + ".Tests/TestResults/coverage.opencover.xml")
+    os.remove(os.curdir + "/" + bootstrap_name + ".Tests/TestResults/Coverage/Reports/Summary.txt")
+    print('Finished')
+    return coverage
+
+# Build data
+def build_data():
+    clean_command = 'dotnet clean | '
+    build_command = 'dotnet build '
+    warnings = '| grep -e "Warning(s)" '
+    errors = '-e "Error(s)" '
+    time = '-e "Time Elapsed"'
+    result = os.popen(clean_command + build_command + warnings + errors + time).readlines()
+    data = Build(result.pop().strip(), result.pop().strip(), result.pop().strip())
+    return data
+
+# Main function
+def run():
+    complexity = calculate_complexity()
+    direct_dependencies = get_direct_dependencies()
+    coverage = float(code_coverage().strip().split(' ').pop(2).replace('%', ''))
+    data = build_data()
+    print(Fonts.BOLD + Fonts.OKBLUE + 'Results:' + Fonts.ENDC)
+    if complexity > 50:
+        print(Fonts.BOLD + 'Cyclomatic complexity: ' + Fonts.WARNING +
+              str(calculate_complexity()) + ' ---> High complexity')
+    else:
+        print(Fonts.BOLD + 'Cyclomatic complexity: ' +
+              Fonts.OKGREEN + str(calculate_complexity()))
+    print(Fonts.ENDC + Fonts.BOLD + 'Direct dependencies:' + Fonts.ENDC)
+    for dep in direct_dependencies:
+        print('\tProject: ' + str(dep.project_name))
+        for d in dep.direct_dependencies:
+            print('\t\tDepends on: ' + str(d).strip())
+    if coverage < 75:
+        print(Fonts.BOLD + 'Line coverage: ' + Fonts.FAIL + str(coverage) + '%' + Fonts.ENDC)
+    else:
+        print(Fonts.BOLD + 'Line coverage: ' + Fonts.OKGREEN + str(coverage) + '%' + Fonts.ENDC)
+    print(Fonts.BOLD + 'Build data:' + Fonts.ENDC)
+    print('\t' + data.warnings)
+    print('\t' + data.build_errors)
+    print('\t' + data.time_elapsed)
+
+run()
